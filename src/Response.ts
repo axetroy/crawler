65904a59f91f61d784f9105b1b3d1e4b24e6fc89
axetroy <troy450409405@gmail.com>
2019-04-19 11:25:15 +0800

    download resource

--- src/Response.ts
+++ src/Response.ts
@@ -1,15 +1,16 @@
 import { AxiosResponse } from "axios";
 import * as cheerio from "cheerio";
+import * as download from "download";
+import * as fs from "fs-extra";
 
-export interface Response extends AxiosResponse, CheerioSelector {
+export interface Response extends AxiosResponse, CheerioSelector, CheerioAPI {
   download(url: string, filepath: string): Promise<void>;
+  follow(url: string): void;
 }
 
 export function CreateResponse(response: AxiosResponse) {
-  const _$ = cheerio.load(response.data);
-
   const $: Response = function(selector: string) {
-    return _$(selector);
+    return cheerio.load(response.data)(selector);
   };
 
   $.status = response.status;
@@ -18,9 +19,28 @@ export function CreateResponse(response: AxiosResponse) {
   $.headers = response.headers;
   $.data = response.data;
 
-  $.download = async (url: string, filepath: string) => {
-    throw new Error("Not implement download resource.");
+  $.download = (url: string, filepath: string) => {
+    return new Promise((resolve, reject) => {
+      download(url)
+        .pipe(fs.createWriteStream(filepath))
+        .once("error", err => {
+          reject(err);
+        })
+        .once("finish", () => {
+          resolve();
+        });
+    });
   };
+
+  // 跟着跳到下一个链接
+  $.follow = (url: string) => {};
+
+  $.load = cheerio.load.bind(cheerio);
+  $.xml = cheerio.xml.bind(cheerio);
+  $.html = cheerio.html.bind(cheerio);
+  $.parseHTML = cheerio.parseHTML.bind(cheerio);
+  $.root = cheerio.root.bind(cheerio);
+  $.contains = cheerio.contains.bind(cheerio);
 
   return $;
 }
