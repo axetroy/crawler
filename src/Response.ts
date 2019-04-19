2c22df3d0187dd0ffe50c672ac6b58f7c6af70c9
axetroy <troy450409405@gmail.com>
2019-04-19 11:55:31 +0800

    update

--- src/Response.ts
+++ src/Response.ts
@@ -22,6 +22,7 @@ export function CreateResponse(response: AxiosResponse) {
   $.config = response.config;
   $.headers = response.headers;
   $.data = response.data;
+  $.request = response.request;
 
   $.download = (
     url: string,
